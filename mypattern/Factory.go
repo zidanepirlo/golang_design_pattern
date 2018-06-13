f676f891a85f00abb17c9c6e978469ae313d0eb7
yuan <zidanepirlo@163.com>
2018-06-13 10:22:21 +0800

    adjust factory model

--- mypattern/Factory.go
+++ mypattern/Factory.go
@@ -9,6 +9,17 @@ type ComputerAbsFactory interface {
 
 type Computer interface {
 	Configuration() string
+	SetCpu(string)
+	SetOs(string)
+	SetMemory(string)
+	SetHardDisk(string)
+}
+
+type GetProperty interface {
+	getCpu() string
+	getOs() string
+	getMemory() string
+	getHardDisk() string
 }
 
 type Mac struct {
@@ -27,8 +38,57 @@ type ThinkPad struct {
 	hardDisk string
 }
 
+func (thinkpad *ThinkPad) SetCpu(val string){
+	if thinkpad!=nil {
+		thinkpad.cpu = val
+	}
+}
+
+func (thinkpad *ThinkPad) SetOs(val string){
+	if thinkpad!=nil {
+		thinkpad.os = val
+	}
+}
+
+func (thinkpad *ThinkPad) SetMemory(val string){
+	if thinkpad!=nil {
+		thinkpad.memory = val
+	}
+}
+
+func (thinkpad *ThinkPad) SetHardDisk(val string){
+	if thinkpad!=nil {
+		thinkpad.hardDisk = val
+	}
+}
+
+
+func (mac *Mac) SetCpu(val string){
+	if mac!=nil {
+		mac.cpu = val
+	}
+}
+
+func (mac *Mac) SetOs(val string){
+	if mac!=nil {
+		mac.os = val
+	}
+}
+
+func (mac *Mac) SetMemory(val string){
+	if mac!=nil {
+		mac.memory = val
+	}
+}
+
+func (mac *Mac) SetHardDisk(val string){
+	if mac!=nil {
+		mac.hardDisk = val
+	}
+}
+
 func (mac Mac) Configuration() string {
-	
+
 	return "Hi, I'm Mac"+split+mac.cpu+split+mac.os+split+mac.memory+split+mac.hardDisk;
 }
 
